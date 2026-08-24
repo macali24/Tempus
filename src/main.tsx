3b44698bb2ca78eb10258e3eaa08f88d4b70bfc0
macali24 <marcuscount9@gmail.com>
2026-08-23 20:41:22 -0500

    Move provider details into main page

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,6 @@ import './phase1.css';
 import './methodology.css';
 import './phase2.css';
 import './change.css';
+import './inline-profile.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
